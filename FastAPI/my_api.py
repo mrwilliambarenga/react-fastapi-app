dc374cfa7890087f535f0f7d5e4f34ffa2ada891
mrwilliambarenga <williambarenga@gmail.com>
2024-10-16 17:16:39 +0100

    Learn about query parameters

--- FastAPI/my_api.py
+++ FastAPI/my_api.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI, Path
+
 
 app = FastAPI()
 
@@ -11,11 +13,22 @@ students = {
     }
 }
 
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
 
+# Path Parameters   (e.g. /get-student/1)
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(..., description="The ID of the student you want to view.", gt=0, lt=3)):
     return students.get(student_id, "Student not found")
+
+
+# Query Parameters  (e.g. /get-by-name/?name=john)
+@app.get("/get-by-name/")
+def get_student(*, name: str = None):
+    for s_id in students:
+        if students[s_id]["name"] == name:
+            return students[s_id]
+    return "Student not found"
