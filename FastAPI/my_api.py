e4588a4d539f2216027c8d90ba300abb23369178
mrwilliambarenga <williambarenga@gmail.com>
2024-10-17 18:38:05 +0100

    Learn about post and put methods

--- FastAPI/my_api.py
+++ FastAPI/my_api.py
@@ -1,15 +1,28 @@
 from typing import Optional
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    group: str
+
+
+class StudentUpdate(BaseModel):
+    name: str = None
+    age: int = None
+    group: str = None
 
 
 students = {
     1: {
         "name": "john",
         "age": 17,
-        "class": "year 12"
+        "group": "year 12"
     }
 }
 
@@ -32,3 +45,24 @@ def get_student(*, name: str = None):
         if students[s_id]["name"] == name:
             return students[s_id]
     return "Student not found"
+
+
+# Post Method
+@app.post("/create-student/{student_id}")
+def create_student(*, student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student already exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
+
+# Put Method
+@app.put("/update-student/{student_id}")
+def update_student(*, student_id: int, student_update: StudentUpdate):
+    if student_id not in students:
+        return {"Error": "Student doesn't exist"}
+    updates = student_update.model_dump(exclude_unset=True)
+    for k, v in updates.items():
+        students[student_id][k] = v
+    return students[student_id]
